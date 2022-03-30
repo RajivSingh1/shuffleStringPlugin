233bb938b589a674cf9445de04fe580f0c170f55
rajiv.singh4 <rajiv.singh4@aon.com>
2022-03-30 13:51:03 +0530

    Add project files.

---
+++ js/jquery.shuffle-string.js
@@ -0,0 +1,24 @@
+﻿(function ($) {
+
+    $.fn.shuffleString = function () {
+
+
+        return this.each(function () {
+            $(this).text(shuffleString($(this).text()));
+
+                function shuffleString(str) {
+                    var array = str.split("");
+                    for (var index = array.length - 1; index > 0; index--) {
+                        var val = Math.floor(Math.random() * (index + 1));
+                        var temp = array[index];
+                        array[index] = array[val];
+                        array[val] = temp;
+
+                    }
+                    return array.join("");
+                }
+        })
+    }
+
+
+}(jQuery));
